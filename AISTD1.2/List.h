127cc4311af7cc7ede4bdf4664331611cde62f27
VlasovEgor <73856832+VlasovEgor@users.noreply.github.com>
2020-11-03 00:59:43 +0300

    Add files via upload

---
+++ AISTD1.2/List.h
@@ -0,0 +1,67 @@
+#pragma once
+//list class description
+template<typename T>
+class List
+{
+public:
+	List();
+	~List();
+
+	// remove the first item in the list
+	void pop_front();
+
+	// add an item to the end of the list
+	void push_back(T data);
+
+	// clear the list
+	void clear();
+
+	// get the number of items in the list
+	int GetSize() { return Size; }
+
+	// Getting an element by index
+	int at(const int index);
+
+	// add an item to the beginning of the list
+	void push_front(T data);
+
+	// add an item to the list at the specified index
+	void insert(T data, int index);
+
+	// deleting an element in the list at the specified index
+	void removeAt(int index);
+
+	// remove the last item in the list
+	void pop_back();
+
+	// check if the list is empty
+	void IsEmpty();
+
+	// replace the element by index with the passed element
+	void Set(T data, int index);
+
+	// output the list items to the console
+	void PrintToConsole();
+
+	// insert another list into the list, starting at the index
+	void InsertList(List newList, int index);
+
+
+private:
+
+	template<typename T>
+	class Node
+	{
+	public:
+		Node* pNext;
+		T data;
+
+		Node(T data = T(), Node* pNext = nullptr)
+		{
+			this->data = data;
+			this->pNext = pNext;
+		}
+	};
+	int Size;
+	Node<T>* head;
+};
